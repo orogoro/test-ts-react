a5fd323b71344eec02e1504d6753fb366e9454ea
Yurii Dvornichenko <raen98@live.ru>
2022-12-06 19:39:40 +0200

    start create hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,7 +3,29 @@ import React from "react";
 import styles from "./Hero.module.scss";
 
 const Hero: React.FC = () => {
-  return <div className={styles.hero}></div>;
+  return (
+    <div className={styles.hero}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>
+          How to do a Website Audit to <br /> Improve SEO & Conversions
+        </h1>
+        <p className={styles.textHero}>
+          Usability auditing with the Plerdy tool is a comprehensive expert
+          assessment of your website based on a profound study of user behavior
+          registered on it. Plerdy will provide you with crucial data based on
+          which you will be able to run an efficient usability audit of your
+          website, identify and fix SEO issues, and subsequently lead it to
+          higher positions in search.
+        </p>
+        <p className={styles.secondaryText}>
+          Thus, you'll compile a list of flaws in your website interface,
+          navigation, and conversion path that can eventually affect its micro
+          and macro conversion rates.
+        </p>
+      </div>
+      <div className={styles.imagesContainer}></div>
+    </div>
+  );
 };
 
 export default Hero;
---
+++ src/components/reusableComponents/actionLink/ActionLink.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import styles from "./ActionLink.module.scss";
+
+const ActionLink: React.FC = () => {
+  return null;
+};
+
+export default ActionLink;
